d4dff890dc73652a7d36ead96573ef7ccd65bca9
Heitor Iga <33259441+heitoriga@users.noreply.github.com>
2020-04-03 14:47:16 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  digitalData = {
+    site: {
+      nome: 'Banco Pan'
+    },
+    page: {
+      secao: 'CRM',
+      subsecao1: 'LandingPage',
+      estadoLogin: 'NL',
+      nome: 'BP:CRM:LandingCRM:LpPortasAbertas'
+    }
+  };
+  ngOnInit() {
+
+  }
+
+}
